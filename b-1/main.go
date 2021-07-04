4099d606cd08211119a6f75b3cd19965e07d95ee
kumashun8 <tiwo3wa4@gmail.com>
2021-07-04 21:31:25 +0900

    b-1 shiftOnly

---
+++ b-1/main.go
@@ -0,0 +1,35 @@
+// 黒板に NN 個の正の整数 A1,…,ANA1,…,AN が書かれています。
+// すぬけ君は，黒板に書かれている整数がすべて偶数であるとき，次の操作を行うことができます。
+// - 黒板に書かれている整数すべてを，2 で割ったものに置き換える。
+// すぬけ君は最大で何回操作を行うことができるかを求めてください。
+
+package main
+
+import "fmt"
+
+func shiftOnly(nums []int, length int, score int) int {
+	var i int
+	for i = 0; i < length; i++ {
+		if nums[i]%2 != 0 {
+			break
+		}
+		nums[i] = nums[i] / 2
+	}
+
+	if i == length {
+		return shiftOnly(nums, length, score+1)
+	}
+	return score
+}
+
+func main() {
+	var n int
+	fmt.Scan(&n)
+
+	nums := make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Scan(&nums[i])
+	}
+
+	fmt.Println(shiftOnly(nums, n, 0))
+}
